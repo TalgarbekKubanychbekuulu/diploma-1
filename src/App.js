12806242658933e9f43efb05e4f16a02ddae9985
Elaman Imashov <elaman@ima.sh>
2022-04-10 21:47:38 -0400

    Add new pages

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 
+import Home from "./pages/Home";
+import Contacts from "./pages/Contacts";
+import Products from "./pages/Products";
+
 function App() {
   return (
     <div className="App">
       <Nav />
       <Header />
+
+      <Home />
+      <Contacts />
+      <Products />
+
       <Footer />
     </div>
   );
