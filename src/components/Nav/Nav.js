4946b2ab98b7f1c26a9a865847c45483b4599297
Elaman Imashov <elaman@ima.sh>
2022-04-10 21:42:53 -0400

    Add NavItem component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,16 +1,15 @@
 import "./Nav.css";
 import Logo from "../ui/Logo/Logo";
+import NavItem from "./NavItem/NavItem";
 
 function Nav() {
   return (
     <nav className="Nav">
       <Logo />
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/">Categories</a></li>
-        <li><a href="/">About</a></li>
-        <li><a href="/">Products</a></li>
-        <li><a href="/">Contacts</a></li>
+        <NavItem url="/">Home</NavItem>
+        <NavItem url="/">Products</NavItem>
+        <NavItem url="/">Contacts</NavItem>
       </ul>
     </nav>
   );
